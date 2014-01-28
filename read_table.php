ded5752bc25550c996e36f29900a019a07aed268
Panjab Shelke <panjabrshelke@gmail.com>
2014-01-28 13:37:28 -0800

    create tables from files
    This is beta version 0.1, it has readme.txt, read_table.php, index.php
    files and one folder PanjabPHPCSV

---
+++ read_table.php
@@ -0,0 +1,91 @@
+<?php
+
+define('db_name', "PanjabPHPCSV"); // folder name which contains the csv files
+define('db_path', getcwd() . "/" . db_name);
+define('db_host', 'localhost'); // Your Server Name
+define('db_user', 'root'); // User Name of MySQL DATABASE
+define('db_password', ''); // Password for MySQL DATABASE
+
+create_database();
+//Created database function for creating database
+function create_database(){
+  $mysql_handle = mysql_connect(db_host, db_user, db_password);
+  if (!$mysql_handle) { die('Could not connect: ' . mysql_error()); }
+  //$sql = 'CREATE DATABASE ' . db_name;
+  $sql = 'CREATE DATABASE IF NOT EXISTS ' . db_name;
+  echo "<br/>***************************** DATA BASE ************************************<br/><br/>";
+  if (mysql_query($sql, $mysql_handle)) {
+      echo "Database " . db_name . " created successfully <br/>";
+      read_database();
+  } else {
+      echo 'Error creating database: ' . mysql_error() . "\n <br/>";
+  }
+  
+  mysql_close($mysql_handle);
+}
+//Database connection 
+function connect_database(){
+  return mysqli_connect(db_host, db_user, db_password, db_name);// or die("Error " . mysqli_error($link));
+}
+//Read database
+function read_database(){
+  $connection = connect_database();
+  if (is_dir(db_path)) {
+      if ($dh = opendir(db_path)) {
+          while (($file = readdir($dh)) !== false) {
+              if( is_csv( $file ) ) {
+                setup_table( $file, $connection );
+              }
+          }
+          closedir($dh);
+      }
+  }
+  mysqli_close( $connection );
+}
+
+function setup_table( $table, $connection ){
+  $table_name = str_replace(".csv", "", $table);
+  echo "<br/>***************************** TABLE Creation Start *****************************<br/><br/>";
+  echo "\nReading table: $table_name\n <br/>";
+  $table_path = db_path . "/" . $table;
+  if (($handle = fopen($table_path, "r")) !== FALSE) {
+      $row_count = 1;
+      while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+          $num = count($row); // number of columns (comma-seperated values)
+          if($row_count == 1){
+            // column names --> skip
+            create_table( $table_name, $row, $connection );
+            $row_count++; continue;
+          }
+          echo "\ninserting values from row " . ($row_count-1) . " into '$table_name' <br/>";
+          insert_into_table( $table_name, $row, $connection );
+          $row_count++;
+      }
+      fclose($handle);
+  }
+  echo "<br/>***************************** TABLE Creation END *****************************<br/>";
+}
+
+
+function create_table($table_name, $columns, $connection){
+  echo "creating table '$table_name'\n <br/>";
+  echo "adding columns to '$table_name' <br/>";
+  //$query = "CREATE TABLE $table_name(" . implode(' TEXT,', $columns) . " TEXT)" or die("Error in the consult.." . mysqli_error($connection));
+ $query = "CREATE TABLE IF NOT EXISTS $table_name(" . implode(' TEXT,', $columns) . " TEXT)" or die("Error in the consult.." . mysqli_error($connection));
+  mysqli_query($connection, $query);
+}
+
+function insert_into_table($table_name, $values, $connection){
+  $query = "INSERT INTO $table_name VALUES ('". implode("','", $values) . "')" or die("Error in the consult.." . mysqli_error($connection));
+  mysqli_query($connection, $query);
+}
+
+function is_csv($file){
+
+  return (pathinfo($file)["extension"] == "csv");
+  /*
+  	if it's not work use below code
+	$file_path = pathinfo($file);
+	return ($file_path["extension"] == "csv");
+  */
+}
--- readme.txt
+++ readme.txt
@@ -1,1 +1,14 @@
 CSV to MySQL DATABASE using PHP
+===============================
+
+
+*Welcome to the First version 0.1 of the CSV-to-MySQL-using-PHP!. What you see is a beta, please test it out like hell,*
+
+### CSV-to-MySQL-using-PHP 0.1-beta (this one here)
+
+This script is for creating **CSV to MySQL DATABASE and TABLES using PHP**, If you have excel files then just save this files as 'csv' and give the specific name of the files because from that name that create table of the database.
+
+* Create database (the database name give the folder and that folder have the all csv file).
+* Read all files from folder **PanjabPHPCSV**
+* Read file and creat table. that table have name as file name and first row of the file is the column of the table. other row are the records of the table in particular table.
+
